b0b692fd2c04306041eec1ce91a45926e5527ad3
adrouin <amaury.d.pro@proton.me>
2026-06-16 16:46:32 +0200

    feat: middleware d'authentification JWT
    Creation de route middleware pour verification des JWT

---
+++ api/src/middleware/auth.ts
@@ -0,0 +1,21 @@
+import {Request, Response, NextFunction} from "express";
+import jwt from "jsonwebtoken";
+import { error } from "node:console";
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+	const header = req.headers.authorization;
+
+	if(!header || !header.startsWith("Bearer ")) {
+		return res.status(401).json({error: "token manquant"});
+	}
+
+	const token = header.slice(7); //To delete "Bearer"
+
+	try {
+		const payload = jwt.verify(token, process.env.JWT_SECRET!) as { sub : string};
+		req.userId = payload.sub;
+		next();
+	} catch {
+		return res.status(401).json({error: "token invalide ou expiré"});
+	}
+}
